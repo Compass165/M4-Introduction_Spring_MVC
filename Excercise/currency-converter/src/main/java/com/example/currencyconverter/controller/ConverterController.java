ff625859c26a29bcf11d2fc1bba17bce4636532a
Compass <ndphong165@gmail.com>
2022-10-14 21:27:51 +0700

    done BT1

---
+++ Excercise/currency-converter/src/main/java/com/example/currencyconverter/controller/ConverterController.java
@@ -0,0 +1,12 @@
+package com.example.currencyconverter.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ConverterController {
+    @GetMapping("/greeting")
+    public String greeting() {
+        return "index";
+    }
+}
---
+++ Practice/customer-management/src/main/java/com/example/customermanagement/servlet/CustomerServlet.java
@@ -0,0 +1,2 @@
+package com.example.customermanagement.servlet;public class CustomerServlet {
+}
